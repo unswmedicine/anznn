8b7de1b3568b123faa422350d39903b322e299d2
Georgina Edwards <gc_edwards@yahoo.com.au>
2013-10-22 17:29:15 +1100

    Don't store secret token in VCS

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Anznn::Application.config.secret_token = 'bf9b0cbc19a1bb2c8feb004ee362910ba00ba601a153a969e3b9a4ca6ee63117729e968ff074cdece98024076f43a93c6de591d433dc29362d2bf8a5ce5c0cae'
+Anznn::Application.config.secret_token = 'REPLACE_THIS_WITH_REAL_GOOD_RANDOM_STRING'
