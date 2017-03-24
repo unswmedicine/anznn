ff274ce9b21276a831c528ec45a5c9980d668b8a
Shaun Muscat <shaun@intersect.org.au>
2017-03-24 12:02:12 +1100

    ANZARD-2: update helper spec to access helper variable
    - Update syntax of helper spec to match the expected syntax of Rspec Rails 3.5 helper specs

--- spec/helpers/responses_helper_spec.rb
+++ spec/helpers/responses_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 # Specs in this file have access to a helper object that includes
 # the ResponsesHelper. For example:
 #
-# describe ResponsesHelper do
+# describe ResponsesHelper, :type => :helper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       helper.concat_strings("this","that").should == "this that"
@@ -11,7 +11,7 @@ require 'rails_helper'
 #   end
 # end
 
-describe ResponsesHelper do
+describe ResponsesHelper, :type => :helper do
   describe "Generating response page titles" do
     it "should string together survey, baby code and year of reg" do
       response = create(:response, baby_code: "Bcdef", year_of_registration: 2015, survey: create(:survey, name: "My Survey"))
