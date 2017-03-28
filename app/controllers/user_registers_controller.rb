d8b85fae5aafc5a67ebf933a16347b3bb06dc801
Shaun Muscat <shaun@intersect.org.au>
2017-03-28 15:52:14 +1100

    ANZARD-2: enable updating of user first and last name

--- app/controllers/user_registers_controller.rb
+++ app/controllers/user_registers_controller.rb
@@ -31,7 +31,7 @@ class UserRegistersController < Devise::RegistrationsController
   # Override the update method in the RegistrationsController so that we don't require password on update
   # https://github.com/plataformatec/devise/blob/v1.3.4/app/controllers/devise/registrations_controller.rb
   def update
-    if resource.update_attributes(params[resource_name])
+    if resource.update_attributes(params.require(resource_name).permit(:first_name, :last_name))
       set_flash_message :notice, :updated if is_navigational_format?
       sign_in resource_name, resource, bypass: true
       respond_with resource, location: after_update_path_for(resource)
