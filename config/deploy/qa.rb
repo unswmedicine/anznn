6527c1a81b8eb22f97a1a40f289a1ea0aedbed0c
Darshan Nagavara <darshan@intersect.org.au>
2017-04-10 11:28:25 +1000

    Update qa.rb

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,9 +1,9 @@
 # Your HTTP server, Apache/etc
-set :web_server, 'gsw1-anznn-qa.intersect.org.au'
+set :web_server, 'anznn-qa.intersect.org.au'
 # This may be the same as your Web server
-set :app_server, 'gsw1-anznn-qa.intersect.org.au'
+set :app_server, 'anznn-qa.intersect.org.au'
 # This is where Rails migrations will run
-set :db_server,  'gsw1-anznn-qa.intersect.org.au'
+set :db_server,  'anznn-qa.intersect.org.au'
 # The user configured to run the rails app
 
 
