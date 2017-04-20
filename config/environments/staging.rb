71c4d0a9e7d57e6bca732c787c10f90d9be6fd30
Darshan Nagavara <darshan@intersect.org.au>
2017-04-20 11:45:31 +1000

    Update staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,6 @@
 Anznn::Application.configure do
 
-  config.action_mailer.default_url_options = { :host => 'anznn-qa.intersect.org.au' }
+  config.action_mailer.default_url_options = { :host => 'anznn-staging.intersect.org.au' }
 
   # Settings specified here will take precedence over those in config/application.rb
 
